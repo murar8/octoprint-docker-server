5abf5edbc1b902cccc66ed100e804353a58e595e
Lorenzo Murarotto <lnzmrr@gmail.com>
2021-05-10 13:22:11 +0200

    add upgrade script

---
+++ update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# update-docker
+#
+# Upgrade all running docker containers to the latest version.
+#
+
+set -e
+
+cd "$(dirname "$0")"
+
+if [ -e /dev/ttyUSB* ] ; then
+    echo "Error: A serial device is connected, aborting upgrade."
+    exit 1
+fi
+
+echo "Starting upgrade."
+
+docker-compose down
+
+docker-compose build --pull
+docker-compose pull
+
+docker-compose up -d
+
+docker system prune --force
+
+echo "Upgrade completed successfully."
